54b5c323dcad5d66ee699451364f2b5d841ece06
Dmytro Komurko <komur.d@gmail.com>
2024-10-07 20:48:54 +0200

    start react app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+    return (
+        <>
+            <h2>Hello world</h2>
+            <h2 style={{ fontFamily: "Roboto" }}>Hello world</h2>
+            <p>
+                Hello world == привет с норвегии Ææ æ /ɛːˀ/ æ /ɛː/ Øø ø /œːˀ/ ø
+                /øː/ Åå å /ɔːˀ/ å
+            </p>
+            <p style={{ fontFamily: "Roboto" }}>
+                Hello world == привет с норвегии Ææ æ /ɛːˀ/ æ /ɛː/ Øø ø /œːˀ/ ø
+                /øː/ Åå å /ɔːˀ/ å
+            </p>
+        </>
+    );
+}
+
+export default App;
---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+export const colors = {
+    primaryRed: "#B22222", // Тёмно-красный
+    secondaryRed: "#FF6347", // Красный
+    accentRed: "#FF4500", // Оранжево-красный
+    lightRed: "#FF8C00", // Светло-красный
+    blue: "#1E3A8A", // Тёмно-синий
+    lightBlue: "#4F83CC", // Светло-синий
+    teal: "#008080", // Темно-бирюзовый
+    lightTeal: "#2ED8B6", // Светло-бирюзовый
+    background: "#F0F0F0", // Общий фон страницы
+    cardBackground: "#FFFFFF", // Фон карточек
+    dividerColor: "#E0E0E0", // Цвет разделителя
+};
