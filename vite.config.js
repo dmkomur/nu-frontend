f50d4f3d5eb3c84adf403ce4d00f1ccbd123c1d5
Dmytro Komurko <komur.d@gmail.com>
2024-10-29 17:05:20 +0100

    reset changes and add caddyfile and nixpacks

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    build: {
-        minify: false, // отключите минификацию для снижения нагрузки
-        sourcemap: false, // отключите sourcemaps, если они не нужны
-    },
-    server: {
-        host: "0.0.0.0",
-        port: 3000,
-    },
 });
