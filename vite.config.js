e89d962e6c657476a776c0a34cb1c0fb15b5bfb5
Dmytro Komurko <komur.d@gmail.com>
2024-10-29 16:34:57 +0100

    package.json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    build: {
+        minify: false, // отключите минификацию для снижения нагрузки
+        sourcemap: false, // отключите sourcemaps, если они не нужны
+    },
     server: {
         host: "0.0.0.0",
         port: 3000,
